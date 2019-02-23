daa44abdd6d4ceb15b0dc3c0bb0f1278b4839379
Damian Kim <damian.kim@tuangru.com>
2019-02-23 02:22:03 -0800

    GUI/added login area component init

---
+++ web/gui/src/app/login-area/login-area.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'login-area',
+	templateUrl: './login-area.component.html'
+})
+export class LoginAreaComponent {
+	name = 'Angular';
+}
