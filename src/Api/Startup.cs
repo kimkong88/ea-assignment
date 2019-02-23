813c1c451d9155d77898aadc9820444c001cba28
Damian Kim <damian.kim@tuangru.com>
2019-02-22 19:06:46 -0800

    API/added autofac and swagger to the project

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Assignment.Api
 {
@@ -16,9 +21,19 @@ namespace Assignment.Api
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
+		public IServiceProvider ConfigureServices(IServiceCollection services)
 		{
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddOptions();
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddSingleton(Configuration);
+			services.AddCors();
+
+			ConfigureSwaggerServices(services);
+
+			var container = CreateAutofacContainer(services);
+
+			return new AutofacServiceProvider(container);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,15 +43,38 @@ namespace Assignment.Api
 			{
 				app.UseDeveloperExceptionPage();
 			}
-			else
-			{
-				// The default HSTS value is 30 days. You may want to change this for production
-				// scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+			app.UseCors(builder => builder
+					.AllowAnyHeader()
+					.AllowAnyOrigin()
+					.AllowCredentials()
+					.AllowAnyMethod());
 
-			app.UseHttpsRedirection();
 			app.UseMvc();
+
+			app.UseSwagger();
+
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint($"/swagger/v1/swagger.json", "Assignment API");
+			});
+		}
+
+		private static IContainer CreateAutofacContainer(IServiceCollection services)
+		{
+			var builder = new ContainerBuilder();
+			builder.Populate(services);
+			builder.RegisterModule<ApiModule>();
+			var container = builder.Build();
+			return container;
+		}
+
+		private void ConfigureSwaggerServices(IServiceCollection services)
+		{
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "EA Assignment API", Version = "v1" });
+				c.DescribeAllEnumsAsStrings();
+			});
 		}
 	}
 }
