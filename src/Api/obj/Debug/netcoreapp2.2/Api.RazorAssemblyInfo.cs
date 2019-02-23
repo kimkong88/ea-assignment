f14eedf08b93e046d168ffe124ad69567a04f65a
Damian Kim <damian.kim@tuangru.com>
2019-02-22 18:24:43 -0800

    Api/added autofac module and modified file structure

--- src/Api/obj/Debug/netcoreapp2.2/Api.RazorAssemblyInfo.cs
+++ src/Api/obj/Debug/netcoreapp2.2/Api.RazorAssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Api.Views")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.RelatedAssemblyAttribute("Assignment.Api.Views")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorLanguageVersionAttribute("2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorConfigurationNameAttribute("MVC-2.1")]
 [assembly: Microsoft.AspNetCore.Razor.Hosting.RazorExtensionAssemblyNameAttribute("MVC-2.1", "Microsoft.AspNetCore.Mvc.Razor.Extensions")]
