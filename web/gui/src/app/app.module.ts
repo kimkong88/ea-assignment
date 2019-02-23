e50e76e6a906c4784a674ffa6e395d738787527a
Damian Kim <damian.kim@tuangru.com>
2019-02-23 03:09:23 -0800

    GUI/updated angular quickstart

--- web/gui/src/app/app.module.ts
+++ web/gui/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginAreaComponent } from './login-area/login-area.component';
 
 @NgModule({
-	imports: [BrowserModule],
-	declarations: [AppComponent, LoginAreaComponent],
-	bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- web/gui/src/app/login-area/login-area.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'login-area',
-	templateUrl: './login-area.component.html'
-})
-export class LoginAreaComponent {
-	name = 'Angular';
-}
