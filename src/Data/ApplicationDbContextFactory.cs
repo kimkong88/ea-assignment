e6e77bfaa2dbdca9c30a2fea64e4fb8fb9e30766
Damian Kim <damian.kim@tuangru.com>
2019-02-24 02:21:32 -0800

    API/db factory created for migration

---
+++ src/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment.Data
+{
+	public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+	{
+		private const string projectName = "Api";
+
+		public ApplicationDbContext CreateDbContext(string[] args)
+		{
+			var basePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, projectName);
+
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(basePath)
+				.AddJsonFile("appsettings.json")
+				.Build();
+			var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+			optionsBuilder.UseNpgsql(connectionString);
+
+			return new ApplicationDbContext(optionsBuilder.Options);
+		}
+	}
+}
